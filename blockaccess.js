0869c6e3518e46f4a6ea92e29b25a0196e2121c9
Louri <louri.panfiloff@gmail.com>
2025-08-08 12:36:37 +0200

    Adding security to login

---
+++ blockaccess.js
@@ -0,0 +1,37 @@
+const allowedEmails = [
+  'louri.panfiloff@gmail.com'
+];
+
+function parseJwt(token) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+      .join('')
+  );
+  return JSON.parse(jsonPayload);
+}
+
+function checkGoogleAccess() {
+  const token = sessionStorage.getItem('google_jwt');
+  if (!token) {
+    window.location.href = 'index.html';
+    return;
+  }
+
+  const payload = parseJwt(token);
+  const userEmail = payload.email;
+
+  if (!allowedEmails.includes(userEmail)) {
+    alert("⛔ Accès refusé : adresse non autorisée");
+    sessionStorage.removeItem('google_jwt');
+    window.location.href = 'index.html';
+  }
+}
+
+function logoutGoogleAccess() {
+  sessionStorage.removeItem('google_jwt');
+  window.location.href = 'index.html';
+}
